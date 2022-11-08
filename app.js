257d8e8489b6befea64c9c4b1812fc05d0b9cf80
maheshb02 <bedademahesh02@gmail.com>
2022-11-08 16:09:15 +0530

    middleware

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const http = require('http');
 
-const routes = require('./routes')
-const server = http.createServer(routes)
-server.listen(3000)
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="value"><button type="submit">submit</button></form>')
+
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+
+})
+
+app.use('/',(req,res,next)=>{
+   res.send('<h1>hello</h1>')
+
+})
+
+app.listen(3000)
