828eef4401d58325e32f331e27c4136d05f08ab0
maheshb02 <bedademahesh02@gmail.com>
2022-11-09 10:19:10 +0530

    split the routes files

--- app.js
+++ app.js
@@ -6,23 +6,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next)=>{
+app.use(adminRoutes);
 
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="value"><button type="submit">submit</button></form>')
+app.use(shopRoutes);
 
-})
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-
-})
-
-app.use('/',(req,res,next)=>{
-   res.send('<h1>hello</h1>')
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>');
 })
 
 app.listen(3000)
